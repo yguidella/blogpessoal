dfb6bc5bb1dacd8024e49d1de005ead3a636b05c
yguidella <y.guidella8@outlook.com>
2026-04-10 16:54:10 -0300

    feat: relacionamento ManyToOne Postagem com Usuario

--- src/postagem/entities/postagem.entity.ts
+++ src/postagem/entities/postagem.entity.ts
@@ -3,6 +3,7 @@
 import { IsNotEmpty } from 'class-validator';
 import { Column, Entity, PrimaryGeneratedColumn, ManyToOne, UpdateDateColumn } from 'typeorm';
 import { Tema } from "../../tema/entities/tema.entity"; 
+import { Usuario } from "../../usuario/entities/usuario.entity"
 
 @Entity({ name: 'tb_postagem' })
 export class Postagem {
@@ -25,4 +26,9 @@ export class Postagem {
         onDelete: "CASCADE"
     })
     tema: Tema;
+
+    @ManyToOne(() => Usuario, (usuario) => usuario.postagem, {
+        onDelete: "CASCADE"
+    })
+    usuario: Usuario
 }
