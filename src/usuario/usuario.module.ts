0639808a796a6c03303970f69726d015f6707436
yguidella <y.guidella8@outlook.com>
2026-04-13 01:15:29 -0300

    feat: configuracao completa do UsuarioModule

--- src/usuario/usuario.module.ts
+++ src/usuario/usuario.module.ts
@@ -1,11 +1,19 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Usuario } from './entities/usuario.entity'; 
+import { AuthModule } from '../auth/auth.module';
+import { UsuarioService } from "./services/usuario.service";
+import { UsuarioController } from "./controllers/usuario.controller";
 
 @Module({
-    imports: [TypeOrmModule.forFeature([Usuario])],
-    providers: [],
-    controllers: [],
-    exports: [TypeOrmModule],
+    imports: [
+        TypeOrmModule.forFeature([Usuario]),
+        forwardRef(() => AuthModule)
+    ],
+
+    providers: [UsuarioService],
+    controllers: [UsuarioController],
+    exports: [UsuarioService],
 })
+
 export class UsuarioModule {}
