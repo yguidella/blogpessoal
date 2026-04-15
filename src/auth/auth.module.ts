4fdad1da8e4b672bd86fda5cf60ff3ea88339f1a
yguidella <y.guidella8@outlook.com>
2026-04-15 15:44:39 -0300

    feat: login e geração de token jwt

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,10 +1,24 @@
-import { Module } from "@nestjs/common";
+import { forwardRef, Module } from "@nestjs/common";
 import { Bcrypt } from "./bcrypt/bcrypt";
+import { UsuarioModule } from "../usuario/usuario.module";
+import { PassportModule } from "@nestjs/passport";
+import { JwtModule } from "@nestjs/jwt";
+import { jwtConstants } from "./constants/constants";
+import { AuthService } from "./services/auth.service";
+import { AuthController } from "./controllers/auth.controller";
+import { LocalStrategy } from "./strategy/local.strategy";
 
 @Module({
-    imports: [],
-    providers: [Bcrypt],
-    controllers: [],
+    imports: [
+        forwardRef(() => UsuarioModule),
+        PassportModule,
+        JwtModule.register({
+            secret: jwtConstants.secret,
+            signOptions: { expiresIn: "1h" },
+        })
+    ],
+    controllers: [AuthController],
+    providers: [Bcrypt, AuthService, LocalStrategy],
     exports: [Bcrypt],
 })
 export class AuthModule {};
--- src/auth/entities/usuariologin.entity.ts
+++ src/auth/entities/usuariologin.entity.ts
@@ -1,4 +1,4 @@
-export class Usuariologin{
+export class UsuarioLogin{
     public usuario: string
     public senha: string
 }
