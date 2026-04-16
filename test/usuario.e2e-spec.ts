2f10edd663d0117bf29291abca5e452c87f8631f
yguidella <y.guidella8@outlook.com>
2026-04-16 10:23:48 -0300

    config: setup inicial do jest e dependencias

---
+++ test/usuario.e2e-spec.ts
@@ -0,0 +1,34 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
+import * as request from 'supertest';
+import { AppModule } from './../src/app.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+describe('Testes dos Módulos Usuário e Auth (e2e)', () => {
+  let app: INestApplication;
+
+  beforeAll(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [
+        TypeOrmModule.forRoot({
+          type: "sqlite",
+          database: ":memory:",
+          entities: [__dirname + "/../src/**/entities/*.entity.ts"],
+          synchronize: true,
+          dropSchema: true
+        }),
+        AppModule
+      ],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+    
+    app.useGlobalPipes(new ValidationPipe());
+    
+    await app.init();
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+});
