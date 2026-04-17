3a26c14d550ec1f6586034148365881c2c6684c1
yguidella <y.guidella8@outlook.com>
2026-04-16 21:13:18 -0300

    test: implementado e validado 5 testes

--- test/usuario.e2e-spec.ts
+++ test/usuario.e2e-spec.ts
@@ -5,6 +5,8 @@ import { AppModule } from './../src/app.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 describe('Testes dos Módulos Usuário e Auth (e2e)', () => {
+  let token: any;
+  let usuarioId: any; 
   let app: INestApplication;
 
   beforeAll(async () => {
@@ -30,5 +32,67 @@ describe('Testes dos Módulos Usuário e Auth (e2e)', () => {
 
   afterAll(async () => {
     await app.close();
+  });
+
+  it("01 - Deve Cadastrar um novo Usuário", async () => {
+    const resposta = await request(app.getHttpServer())
+      .post('/usuarios/cadastrar')
+      .send({
+        nome: 'Root',
+        usuario: 'root@root.com',
+        senha: 'rootroot',
+        foto: '-',
+      })
+      .expect(201);
+
+    usuarioId = resposta.body.id;
+  });
+
+  it("02 - Não Deve Cadastrar um Usuário Duplicado", async () => {
+    await request(app.getHttpServer())
+      .post('/usuarios/cadastrar')
+      .send({
+        nome: 'Root',
+        usuario: 'root@root.com',
+        senha: 'rootroot',
+        foto: '-',
+      })
+      .expect(400);
+  });
+
+  it("03 - Deve Autenticar o Usuário (Login)", async () => {
+    const resposta = await request(app.getHttpServer())
+    .post("/usuarios/logar")
+    .send({
+      usuario: 'root@root.com',
+      senha: 'rootroot',
+    })
+    .expect(200);
+    token = resposta.body.token;
+  });
+
+  it("04 - Deve Listar todos os Usuários", async () => {
+    return request(app.getHttpServer())
+    .get('/usuarios/all')
+    .set('Authorization', `${token}`)
+    .send({})
+    .expect(200);
+  });
+
+  it("05 - Deve Atualizar um Usuário", async () => {
+    return request(app.getHttpServer())
+    .put('/usuarios/atualizar')
+    .set('Authorization', `${token}`)
+    .send({
+      id: usuarioId,
+      nome: 'Root Atualizado',
+      usuario: 'root@root.com',
+      senha: 'rootroot',
+      foto: '-',
+    })
+    .expect(200)
+    .then( resposta => {
+      expect("Root Atualizado").toEqual(resposta.body.nome);
+    });
   });
 });
