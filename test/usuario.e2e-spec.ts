5464d9db9a1bf1a3a256c0afba025c3130628b34
yguidella <y.guidella8@outlook.com>
2026-04-17 18:36:38 -0300

    test: testes e2e de usuario

--- test/usuario.e2e-spec.ts
+++ test/usuario.e2e-spec.ts
@@ -1,6 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication, ValidationPipe } from '@nestjs/common';
-import * as request from 'supertest';
+import request from 'supertest';
 import { AppModule } from './../src/app.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
