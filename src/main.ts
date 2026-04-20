26d89322db0292c48c2b945b03a6212786047873
yguidella <y.guidella8@outlook.com>
2026-04-20 11:59:10 -0300

    fix: ajuste porta main.ts

--- src/main.ts
+++ src/main.ts
@@ -22,6 +22,6 @@ async function bootstrap() {
 
   SwaggerModule.setup('/swagger', app, document);
 
-  await app.listen(process.env.PORT ?? 4000); 
+  await app.listen(process.env.PORT || 4000);
 }
 void bootstrap();
